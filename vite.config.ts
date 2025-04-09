062e02d5f12ca9bf4e4181e63433b65138404c7a
Cikal Taruna <cikaltaruna@gmail.com>
2025-04-09 21:52:58 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [
     react(),
   ],
-  base: "/itsme",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+  base: "/itsme"
+});
