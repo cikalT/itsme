e9c077fa14f58e774c29a71d16a8976b3391035c
Cikal Taruna <cikaltaruna@gmail.com>
2025-04-10 23:10:05 +0800

    up path error

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: '/itsme/',
-  server: {
-    host: '::',
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.', // copies to dist/404.html
+          rename: '404.html',
+        },
+      ],
+    }),
+  ],
 });
